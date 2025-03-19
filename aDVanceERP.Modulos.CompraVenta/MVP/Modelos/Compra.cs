2a074fd021c1c78d12ebf46f0a07fb0a2fcc0bad
Carlos Davison Gomez <cdavisongmz@yahoo.com>
2025-03-19 00:22:49 -0400

    Create Compra.cs
    Creado el nuevo modelo para compras

---
+++ aDVanceERP.Modulos.CompraVenta/MVP/Modelos/Compra.cs
@@ -0,0 +1,40 @@
+﻿using aDVanceERP.Core.MVP.Modelos.Plantillas;
+
+namespace aDVanceERP.Modulos.CompraVenta.MVP.Modelos {
+    public class Compra : IObjetoUnico {
+        public Compra() {
+        }
+
+        public Compra(long id, DateTime fecha, long idAlmacen, long idProveedor, float total) {
+            Id = id;
+            Fecha = fecha;
+            IdAlmacen = idAlmacen;
+            IdProveedor = idProveedor;
+            Total = total;
+        }
+
+        public long Id { get; set; }
+        public DateTime Fecha { get; set; }
+        public long IdAlmacen { get; set; }
+        public long IdProveedor { get; set; }
+        public float Total { get; set; }
+    }
+
+    public enum CriterioBusquedaCompra {
+        Todos,
+        Id,
+        NombreAlmacen,
+        RazonSocialProveedor,
+        Fecha
+    }
+
+    public static class UtilesBusquedaCompra {
+        public static string[] CriterioBusquedaCompra = new string[] {
+            "Todas las compras",
+            "Identificador de BD",
+            "Nombre del almacén",
+            "Razón social del proveedor",
+            "Fecha de la compra"
+        };
+    }
+}
