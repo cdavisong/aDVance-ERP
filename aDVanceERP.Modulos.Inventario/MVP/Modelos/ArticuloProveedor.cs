abd072fc62e5a68b5390bd12968d651bf1aec4c8
Carlos Davison Gomez <cdavisongmz@yahoo.com>
2025-03-19 00:31:04 -0400

    Create ArticuloProveedor.cs
    Creado el nuevo modelo que relaciona los articulos con los proveedores. El modelo almacenará los precios de adquisición y cesión específicos por proveedor para cada artículo, permitiendo poder registrar el mismo articulo con precios de adquisición independientes por proveedor.

---
+++ aDVanceERP.Modulos.Inventario/MVP/Modelos/ArticuloProveedor.cs
@@ -0,0 +1,33 @@
+﻿using aDVanceERP.Core.MVP.Modelos.Plantillas;
+
+namespace aDVanceERP.Modulos.Inventario.MVP.Modelos {
+    public class ArticuloProveedor : IObjetoUnico {
+        public ArticuloProveedor() {
+        }
+
+        public ArticuloProveedor(long id, long idProveedor, float precioAdquisicion, float precioVenta) {
+            Id = id;
+            IdProveedor = idProveedor;
+            PrecioAdquisicion = precioAdquisicion;
+            PrecioVenta = precioVenta;
+        }
+
+        public long Id { get; set; }
+        public long IdArticulo { get; set; }
+        public long IdProveedor { get; set; }
+        public float PrecioAdquisicion { get; }
+        public float PrecioVenta { get; }
+    }
+
+    public enum CriterioBusquedaArticuloProveedor {
+        Todos,
+        Id
+    }
+
+    public static class UtilesBusquedaArticuloProveedor {
+        public static string[] CriterioBusquedaArticulo = new string[] {
+            "Todos los artículos",
+            "Identificador de BD"
+        };
+    }
+}
