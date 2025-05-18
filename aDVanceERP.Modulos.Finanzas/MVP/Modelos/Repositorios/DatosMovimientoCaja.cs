77f0073fa7bc6282909ecafa3dfe4fb820045ed2
Carlos Davison Gomez <cdavisongmz@yahoo.com>
2025-05-18 19:54:13 -0400

    Modificaciones en la obtención de datos de movimiento de caja

--- aDVanceERP.Modulos.Finanzas/MVP/Modelos/Repositorios/DatosMovimientoCaja.cs
+++ aDVanceERP.Modulos.Finanzas/MVP/Modelos/Repositorios/DatosMovimientoCaja.cs
@@ -17,7 +17,7 @@ namespace aDVanceERP.Modulos.Finanzas.MVP.Modelos.Repositorios {
                         {objeto.IdCaja},
                         '{objeto.Fecha:yyyy-MM-dd HH:mm:ss}',
                         {objeto.Monto},
-                        {(int) objeto.Tipo},
+                        '{objeto.Tipo}',
                         '{objeto.Concepto}',
                         {objeto.IdPago},
                         {objeto.IdUsuario},
@@ -32,7 +32,7 @@ namespace aDVanceERP.Modulos.Finanzas.MVP.Modelos.Repositorios {
                     SET
                         fecha='{objeto.Fecha:yyyy-MM-dd HH:mm:ss}',
                         monto={objeto.Monto},
-                        tipo={(int) objeto.Tipo},
+                        tipo='{(int) objeto.Tipo}',
                         concepto='{objeto.Concepto}',
                         observaciones='{objeto.Observaciones}'
                     WHERE id_movimiento_caja={objeto.Id};
@@ -47,7 +47,7 @@ namespace aDVanceERP.Modulos.Finanzas.MVP.Modelos.Repositorios {
             return criterio switch {
                 CriterioBusquedaMovimientoCaja.Id => $"SELECT * FROM adv__movimiento_caja WHERE id_movimiento_caja={dato};",
                 CriterioBusquedaMovimientoCaja.Fecha => $"SELECT * FROM adv__movimiento_caja WHERE DATE(fecha) = '{dato}';",
-                CriterioBusquedaMovimientoCaja.Tipo => $"SELECT * FROM adv__movimiento_caja WHERE tipo={dato};",
+                CriterioBusquedaMovimientoCaja.Tipo => $"SELECT * FROM adv__movimiento_caja WHERE tipo='{dato}';",
                 CriterioBusquedaMovimientoCaja.Concepto => $"SELECT * FROM adv__movimiento_caja WHERE concepto LIKE '%{dato}%';",
                 _ => "SELECT * FROM adv__movimiento_caja;"
             };
@@ -59,7 +59,7 @@ namespace aDVanceERP.Modulos.Finanzas.MVP.Modelos.Repositorios {
                 lectorDatos.GetInt32(lectorDatos.GetOrdinal("id_caja")),
                 lectorDatos.GetDateTime(lectorDatos.GetOrdinal("fecha")),
                 lectorDatos.GetDecimal(lectorDatos.GetOrdinal("monto")),
-                (TipoMovimientoCaja) lectorDatos.GetInt32(lectorDatos.GetOrdinal("tipo")),
+                (TipoMovimientoCaja) Enum.Parse(typeof(EstadoCaja), lectorDatos.GetString(lectorDatos.GetOrdinal("tipo"))),
                 lectorDatos.IsDBNull(lectorDatos.GetOrdinal("concepto")) ? null : lectorDatos.GetString(lectorDatos.GetOrdinal("concepto")),
                 lectorDatos.IsDBNull(lectorDatos.GetOrdinal("id_pago")) ? 0 : lectorDatos.GetInt32(lectorDatos.GetOrdinal("id_pago")),
                 lectorDatos.IsDBNull(lectorDatos.GetOrdinal("id_usuario")) ? 0 : lectorDatos.GetInt32(lectorDatos.GetOrdinal("id_usuario")),
