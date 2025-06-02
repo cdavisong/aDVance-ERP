03930a3605249757f05fccb81d4bc0b9e67c1d1e
Carlos Davison Gomez <cdavisongmz@yahoo.com>
2025-06-01 23:32:38 -0400

    Definidos los permisos del módulo taller

---
+++ aDVanceERP.Modulos.Taller/ModuloTaller.cs
@@ -0,0 +1,17 @@
+﻿namespace aDVanceERP.Modulos.Taller;
+
+public static class ModuloTaller {
+    public const string Nombre = "MOD_TALLER";
+
+    public static readonly string[] Permisos = {
+        "MOD_TALLER_TODOS",
+        "MOD_TALLER_ACTIVIDADES_TODOS",
+        "MOD_TALLER_ACTIVIDADES_ADICIONAR",
+        "MOD_TALLER_ACTIVIDADES_EDITAR",
+        "MOD_TALLER_ACTIVIDADES_ELIMINAR",
+        "MOD_TALLER_ORDENES_TRABAJO_TODOS",
+        "MOD_TALLER_ORDENES_TRABAJO_ADICIONAR",
+        "MOD_TALLER_ORDENES_TRABAJO_EDITAR",
+        "MOD_TALLER_ORDENES_TRABAJO_ELIMINAR"
+    };
+}
