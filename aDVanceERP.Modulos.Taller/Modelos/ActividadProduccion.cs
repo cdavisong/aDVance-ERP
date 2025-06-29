0827fa1a8b3769e8e3ebd1ea47ef73cf7b864def
Carlos Davison Gomez <cdavisongmz@yahoo.com>
2025-06-28 23:15:13 -0400

    implementacion de taller

--- aDVanceERP.Modulos.Taller/Modelos/ActividadProduccion.cs
+++ aDVanceERP.Modulos.Taller/Modelos/ActividadProduccion.cs
@@ -1,6 +1,6 @@
 ﻿using aDVanceERP.Core.MVP.Modelos.Plantillas;
 
-namespace aDVanceERP.Modulos.Taller.MVP.Modelos;
+namespace aDVanceERP.Modulos.Taller.Modelos;
 
 public class ActividadProduccion : IObjetoUnico {
     public ActividadProduccion() {
