46ffa03aaec1f01901613156796eecf53a511e28
cdavisong <carlosdavison@nauta.cu>
2025-08-13 21:26:44 -0400

    IRepoEntidadBaseDatos nombres de variables en funciones actualizadas

--- aDVanceERP.Core/Repositorios/Interfaces/IRepoEntidadBaseDatos.cs
+++ aDVanceERP.Core/Repositorios/Interfaces/IRepoEntidadBaseDatos.cs
@@ -11,7 +11,7 @@ public interface IRepoEntidadBaseDatos<En, Fb> : IDisposable
     long Adicionar(En objeto);
     bool Editar(En objeto, long nuevoId = 0);
     bool Eliminar(long id);
-    (int cantidad, IEnumerable<En> resultados) Obtener(string? textoComando = "", int limite = 0, int desplazamiento = 0);
-    (int cantidad, IEnumerable<En> resultados) Obtener(Fb? criterio, string? dato, int limite = 0, int desplazamiento = 0);
+    (int cantidad, IEnumerable<En> resultados) Obtener(string? consulta = "", int limite = 0, int desplazamiento = 0);
+    (int cantidad, IEnumerable<En> resultados) Obtener(Fb? filtroBusqueda, string? criterio, int limite = 0, int desplazamiento = 0);
     bool Existe(string dato);
 }
